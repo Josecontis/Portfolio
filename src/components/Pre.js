00e8509d5b231c1ab4f963183bc26f8dd7be7fd5
Giuseppe Conticchio <giuseppe.conticchio@macnil.local>
2021-09-02 17:32:50 +0200

    First personal website version

---
+++ src/components/Pre.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Pre(props) {
+  return (
+    <>
+      <div class="shape-blob one"></div>
+      <div class="shape-blob two"></div>
+      <div class="shape-blob three"></div>
+      <div class="shape-blob four"></div>
+      <div class="shape-blob five"></div>
+      <div class="shape-blob six"></div>
+    </>);
+}
+
+export default Pre;
